664c493b942aeba62639214afc970439a924dce9
Eugene Kubesh <codbasters@gmail.com>
2022-10-24 11:14:59 +0200

    add validation for requested permissions

--- src/app/helpers/prepare-requested-permissions.ts
+++ src/app/helpers/prepare-requested-permissions.ts
@@ -2,5 +2,12 @@ import { AclObjectPermission } from '../interfaces/acl-object-permission';
 import { AclDirectivePermissions } from '../interfaces/acl-directive-permissions';
 
 export function prepareRequestedPermissions(value: AclDirectivePermissions): string[] | AclObjectPermission[] {
-  return Array.isArray(value) ? value : [value as any];
+  const requestedPermissions = Array.isArray(value) ? value : [value as any];
+
+  return requestedPermissions.filter((permission) => {
+    const stringEntry = typeof permission === 'string';
+    const objectEntry = typeof permission === 'object' && typeof permission?.permission === 'string'
+
+    return stringEntry || objectEntry;
+  })
 }
