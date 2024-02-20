7e253935dbe4bd1a24679ddb5c4d5ea0fe02a38a
Raymond Gigliotti <ray@firestitch.com>
2024-02-20 07:15:18 -0500

    Unsupported private class

--- src/public_api.ts
+++ src/public_api.ts
@@ -4,14 +4,18 @@ export { FsAcl } from './app/services/acl.service';
 export { AclAccess } from './app/enums/acl-access.enum';
 export { AclRequire } from './app/enums/acl-require.enum';
 
-export { AclEntry } from './app/interfaces/acl-entry';
 export { AclAccesses } from './app/consts/accesses';
+export { AclEntry } from './app/interfaces/acl-entry';
 
 export { AclObjectPermission } from './app/interfaces/acl-object-permission';
 export { AclPermissionParam } from './app/interfaces/acl-permission-param';
 
-export { AclReadDirective } from './app/directives/structured/acl-read.directive';
-export { AclWriteDirective } from './app/directives/structured/acl-write.directive';
-export { AclFullDirective } from './app/directives/structured/acl-full.directive';
 export { AclDisableDirective } from './app/directives/attributed/acl-disable.directive';
 export { AclEnableDirective } from './app/directives/attributed/acl-enable.directive';
+export { AclFullDirective } from './app/directives/structured/acl-full.directive';
+export { AclNotFullDirective } from './app/directives/structured/acl-not-full.directive';
+export { AclNotReadDirective } from './app/directives/structured/acl-not-read.directive';
+export { AclNotWriteDirective } from './app/directives/structured/acl-not-write.directive';
+export { AclReadDirective } from './app/directives/structured/acl-read.directive';
+export { AclWriteDirective } from './app/directives/structured/acl-write.directive';
+
